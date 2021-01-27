9ad6d8d334ab005ee749595ef9dc964b7e1c7704
Maartje Eyskens <maartje@eyskens.me>
2021-01-27 17:49:55 +0100

    Log the time
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- print.py
+++ print.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 import json
 
 import logging
-logging.basicConfig(filename='voeding.log', level=logging.DEBUG)
+logging.basicConfig(filename='voeding.log', level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 from threading import Lock
 
